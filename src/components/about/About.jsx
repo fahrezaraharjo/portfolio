afd65053ddb7fcdff5404c55a0aa1d8cecd0501d
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-04-08 22:29:23 +0700

    content about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import "./about.css"
+import Me from "../../assets/eja-about.PNG"
 
 
 const about = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      
+      <div className="countainer about__container">
+        <div className='about__me'>
+        <div className="about__me-image">
+          <img src={Me} alt="About Image" />
+        </div>
+        </div>
+
+        <div className="about__content">
+        <div className="about__card">
+          <article className='about__card'>
+
+          </article>
+        </div>
+        </div>
+      </div>
+      </section>
   )
 }
 
