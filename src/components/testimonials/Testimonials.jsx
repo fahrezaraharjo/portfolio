52b974d014f7d6dc3a92f0a1b6c5bd8ad93e9011
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-04-12 14:48:40 +0700

    using swiper for testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,19 +6,21 @@ import AVTR3 from "../../assets/avatar3.jpg"
 import AVTR4 from "../../assets/avatar4.jpg"
 
 // import Swiper core and required modules
-import { Pagination } from 'swiper';
+import { Navigation, Pagination,} from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
 
 const data = [
   {
     avatar: AVTR1,
     name: "Tina Snow",
-    review: "Awesome"
+    review: "Modi alias animi dolorem aliquam ea eumbeatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consecuatur qui molestiae, Dolorem, perpspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?"
   },
   {
     avatar: AVTR2,
@@ -43,7 +45,18 @@ const Testimonials = () => {
       <h5>Review from client</h5>
       <h2>Testimonials</h2>
 
-      <Swiper className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+        // install Swiper modules
+        modules={[Navigation, Pagination,]}
+        spaceBetween={70}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        
+        
+        >
+
+
         {
           data.map(({ avatar, name, review }, index) => {
             return (
