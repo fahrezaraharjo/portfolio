886ee7f3ee17a21d68c1476b1d226c2c4af2f55e
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-04-12 13:45:55 +0700

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 import "./testimonials.css"
+import AVTR1 from "../../assets/avatar1.jpg"
+import AVTR2 from "../../assets/avatar2.jpg"
+import AVTR3 from "../../assets/avatar3.jpg"
+import AVTR4 from "../../assets/avatar4.jpg"
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Tina Snow",
+    review: "Awesome"
+  },
+  {
+    avatar: AVTR2,
+    name: "Shata Wale",
+    review: "Modi alias animi dolorem aliquam ea eumbeatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consecuatur qui molestiae, Dolorem, perpspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?"
+  },
+  {
+    avatar: AVTR3,
+    name: "Kwame Despite",
+    review: "Modi alias animi dolorem aliquam ea eumbeatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consecuatur qui molestiae, Dolorem, perpspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?"
+  },
+  {
+    avatar: AVTR4,
+    name: "Nana Ama Mcbrown",
+    review: "Modi alias animi dolorem aliquam ea eumbeatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consecuatur qui molestiae, Dolorem, perpspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?"
+  },
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonial'>Testimonials</section>
+    <section id='testimonial'>
+      <h5>Review from client</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container">
+        {
+          data.map(({ avatar, name, review }, index) => {
+            return (
+              <SwiperSlide key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} />
+                  <h5 className='client__name'>{name}</h5>
+                </div>
+                <small className='client__review'>{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
