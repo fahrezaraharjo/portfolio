41531fa8147aedc78d016ce17bd6580d19678f8e
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-04-13 16:45:00 +0700

    Done project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import "./footer.css"
+import {MdOutlineFacebook} from 'react-icons/md'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Arcadiacode</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Service</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><MdOutlineFacebook/></a>
+        <a href="https://instagram.com"><AiOutlineInstagram /></a>
+        <a href="https://twitter.com"><AiOutlineTwitter /></a>
+      </div>
+
+    <div className="footer__copyright">
+      <small>&copy; ARCADIA Code. All rigth reserved. </small>
+    </div>
+    </footer>
   )
 }
 
